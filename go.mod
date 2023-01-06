8bc790dcbab83c382210dd3e447f945a9da9c130
Alberto Benatti <albi.benni8@gmail.com>
2023-01-06 17:40:50 +0100

    init w main

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golangLearningProject
+
+go 1.19
---
+++ main/basics.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var stringToDeclare = "Name"
+	fmt.Println(`something to print `, stringToDeclare)
+}
