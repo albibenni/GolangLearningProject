be9ba2ba2f64439413cf70617fa9c9e23004e7fc
Alberto Benatti <albi.benni8@gmail.com>
2023-01-07 13:54:09 +0100

    comments

--- main/basics.go
+++ main/basics.go
@@ -11,13 +11,17 @@ func main() {
 	fmt.Println(`something to print `, stringToDeclare, whatWasSaid, whatWasSaidTwice, theOther)
 }
 
+// generic function
 func saySomething() string {
 	return "Something"
 }
+
+// double type return
 func saySomethingTwice() (string, string) {
 	return "Something", "Else"
 }
 
+// pointers
 func pointerChange(s *string) {
 	newValue := "Value changed"
 	*s = newValue
