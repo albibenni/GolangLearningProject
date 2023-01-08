938078bfa42f385c0f62f9ee11250e2fc6db68ef
Alberto Benatti <albi.benni8@gmail.com>
2023-01-08 11:33:42 +0100

    Data structures: slices

--- main/structures.go
+++ main/structures.go
@@ -1,6 +1,9 @@
 package main
 
-import "log"
+import (
+	"log"
+	"sort"
+)
 
 func main() {
 	log.Println("Something")
@@ -17,4 +20,19 @@ func main() {
 
 	log.Println(myMap["key"])
 	log.Println(secondMap["me"].FirstName)
+
+	mySlice := makeASlice(me)
+	log.Println(mySlice)
+
+	numbers := []int{10, 2, 8, 4, 5}
+	sort.Ints(numbers)
+	log.Println(numbers)
+	log.Println(numbers[0:3])
+}
+
+func makeASlice(me SimpleUser) []SimpleUser {
+	var mySlice []SimpleUser
+
+	mySlice = append(mySlice, me)
+	return mySlice
 }
