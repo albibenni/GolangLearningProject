e8d7f7f4910a38a501ad8dad83a3efe26b9e1d26
Alberto Benatti <albi.benni8@gmail.com>
2023-01-10 21:34:23 +0100

    Loop, forRange with _ and maps

--- main/particularLoops.go
+++ main/particularLoops.go
@@ -3,8 +3,14 @@ package main
 import "log"
 
 func main() {
-
 	testForRangeLoop()
+	log.Println()
+	testForRangeLoopUnderscore()
+	log.Println()
+	log.Println()
+	testForRangeLoopUnderscoreWMap()
+	log.Println()
+	testForRangeLoopWMap()
 }
 
 func testForRangeLoop() {
@@ -12,5 +18,35 @@ func testForRangeLoop() {
 
 	for i, animal := range animals {
 		log.Println(i, animal)
+	}
+}
+
+// _ you dont care about the variable
+func testForRangeLoopUnderscore() {
+	animals := []string{"dog", "fish", "horse", "cow", "cat"}
+
+	for _, animal := range animals {
+		log.Println(animal)
+	}
+}
+
+func testForRangeLoopUnderscoreWMap() {
+	animals := make(map[string]string)
+
+	animals["dog"] = "doggyName"
+	animals["cat"] = "catName"
+
+	for _, animal := range animals {
+		log.Println(animal)
+	}
+}
+func testForRangeLoopWMap() {
+	animals := make(map[string]string)
+
+	animals["dog"] = "doggyName"
+	animals["cat"] = "catName"
+
+	for animalKind, animal := range animals {
+		log.Println(animalKind, animal)
 	}
 }
